73c16eaf6e925d4b4e5f755d7790fdb974402332
Benoit Bovy <benbovy@gmail.com>
2020-10-25 02:25:53 +0100

    wip query return distances + index serialization
    Serialization is not optimized at all. The whole index has to be rebuilt
    from the original points that are saved in addition to the tree data.

--- include/pys2index/s2pointindex.hpp
+++ include/pys2index/s2pointindex.hpp
@@ -1,28 +1,43 @@
 #ifndef __S2POINTINDEX_H_
 #define __S2POINTINDEX_H_
 
+#include <tuple>
+
+#include "pybind11/pybind11.h"
+
 #include "xtensor/xtensor.hpp"
 
 #define FORCE_IMPORT_ARRAY
 #include "xtensor-python/pytensor.hpp"
 
+#include "s2/s2cell_id.h"
 #include "s2/s2closest_point_query.h"
 #include "s2/s2latlng.h"
 #include "s2/s2point.h"
 #include "s2/s2point_index.h"
 
+namespace py = pybind11;
+
 
 class s2point_index
 {
 public:
+    using index_t = S2PointIndex<npy_intp>;
+
     s2point_index(const xt::pytensor<double, 2> &latlon_points);
     s2point_index(const xt::pytensor<float, 2> &latlon_points);
 
+    s2point_index(const xt::pytensor<uint64, 1> &cell_ids);
+
     template <class T>
-    xt::pytensor<npy_intp, 1> query(const xt::pytensor<T, 2> &latlon_points);
+    py::tuple query(const xt::pytensor<T, 2> &latlon_points);
+
+    const xt::pytensor<uint64, 1> get_cell_ids();
+    const xt::pytensor<double, 2> get_latlon_points();
 
 private:
-    S2PointIndex<npy_intp> m_index;
+    index_t m_index;
+    const xt::pytensor<double, 2> m_latlon_points;
 
     template <class T>
     void add_latlon_points(const xt::pytensor<T, 2> &latlon_points);
@@ -30,16 +45,30 @@ private:
 
 
 s2point_index::s2point_index(const xt::pytensor<double, 2> &latlon_points)
+    : m_latlon_points(latlon_points)
 {
     add_latlon_points(latlon_points);
 }
 
 
 s2point_index::s2point_index(const xt::pytensor<float, 2> &latlon_points)
+    : m_latlon_points(latlon_points)
 {
     add_latlon_points(latlon_points);
 }
 
+
+s2point_index::s2point_index(const xt::pytensor<uint64, 1> &cell_ids)
+{
+    auto num_points = cell_ids.size();
+
+    for (std::size_t i=0; i<num_points; ++i)
+    {
+        S2CellId c(cell_ids(i));
+        m_index.Add(c.ToPoint(), static_cast<npy_intp>(i));
+    }
+
+}
 
 template <class T>
 void s2point_index::add_latlon_points(const xt::pytensor<T, 2> &latlon_points)
@@ -55,9 +84,10 @@ void s2point_index::add_latlon_points(const xt::pytensor<T, 2> &latlon_points)
 
 
 template <class T>
-xt::pytensor<npy_intp, 1> s2point_index::query(const xt::pytensor<T, 2> &latlon_points)
+py::tuple s2point_index::query(const xt::pytensor<T, 2> &latlon_points)
 {
     const auto shape = latlon_points.shape();
+    auto results_dists = xt::pytensor<double, 1>::from_shape({shape[0]});
     auto results_idx = xt::pytensor<npy_intp, 1>::from_shape({shape[0]});
 
     S2ClosestPointQuery<npy_intp> query(&m_index);
@@ -69,12 +99,51 @@ xt::pytensor<npy_intp, 1> s2point_index::query(const xt::pytensor<T, 2> &latlon_
 
         auto results = query.FindClosestPoint(&target);
 
+        results_dists(i) = results.distance().radians();
         results_idx(i) = static_cast<npy_intp>(results.data());
     }
 
-    return results_idx;
+    return py::make_tuple(results_dists, results_idx);
 }
 
 
+const xt::pytensor<uint64, 1> s2point_index::get_cell_ids()
+{
+    auto cell_ids = xt::pytensor<uint64, 1>::from_shape({m_index.num_points()});
+
+    index_t::Iterator iter;
+    iter.Init(&m_index);
+
+    std::size_t i = 0;
+
+    for (iter.Begin(); !iter.done(); iter.Next())
+    {
+        cell_ids(i) = iter.id().id();
+        ++i;
+    }
+
+    return cell_ids;
+}
+
+
+const xt::pytensor<double, 2> s2point_index::get_latlon_points()
+{
+    xt::pytensor<double, 2> latlon_points(m_latlon_points);
+
+    // index_t::Iterator iter;
+    // iter.Init(&m_index);
+
+    // std::size_t i = 0;
+
+    // for (iter.Begin(); !iter.done(); iter.Next())
+    // {
+    //     S2LatLng ll(iter.point());
+    //     latlon_points(i, 0) = ll.lat().degrees();
+    //     latlon_points(i, 1) = ll.lng().degrees();
+    //     ++i;
+    // }
+
+    return latlon_points;
+}
 
 #endif // __S2POINTINDEX_H_
--- src/main.cpp
+++ src/main.cpp
@@ -24,25 +24,44 @@ PYBIND11_MODULE(pys2index, m)
            S2PointIndex.query
     )pbdoc";
 
-    py::class_<s2point_index>(m, "S2PointIndex", R"pbdoc(
-            S2 index for fast geographic point problems.
+    py::class_<s2point_index> py_s2pointindex (m, "S2PointIndex", R"pbdoc(
+        S2 index for fast geographic point problems.
 
-            Parameters
-            ----------
-            latlon_points : array-like of shape (n_points, 2)
-                2-d array of point coordinates (latitude, longitude) in degrees.
-        )pbdoc")
-        .def(py::init<const xt::pytensor<double, 2>&>())
-        .def(py::init<const xt::pytensor<float, 2>&>())
-        .def("query", &s2point_index::query<double>, R"pbdoc(
-            Query the index for nearest neighbors.
+        Parameters
+        ----------
+        latlon_points : array-like of shape (n_points, 2)
+            2-d array of point coordinates (latitude, longitude) in degrees.
+    )pbdoc");
 
-            Parameters
-            ----------
-            latlon_points : array-like of shape (n_points, 2)
-                2-d array of query point coordinates (latitude, longitude) in degrees.
-        )pbdoc")
-        .def("query", &s2point_index::query<float>,
-             "Query the index for nearest neighbors (float version).");
+    py_s2pointindex.def(py::init<const xt::pytensor<double, 2>&>(), py::call_guard<py::gil_scoped_release>());
+    py_s2pointindex.def(py::init<const xt::pytensor<float, 2>&>(), py::call_guard<py::gil_scoped_release>());
+    py_s2pointindex.def(py::init<const xt::pytensor<uint64, 1>&>(), py::call_guard<py::gil_scoped_release>());
+
+    py_s2pointindex.def("query", &s2point_index::query<double>, py::call_guard<py::gil_scoped_release>(), R"pbdoc(
+        Query the index for nearest neighbors.
+
+        Parameters
+        ----------
+        latlon_points : array-like of shape (n_points, 2)
+            2-d array of query point coordinates (latitude, longitude) in degrees.
+    )pbdoc");
+    py_s2pointindex.def("query", &s2point_index::query<float>, py::call_guard<py::gil_scoped_release>(),
+        "Query the index for nearest neighbors (float version).");
+
+    py_s2pointindex.def("get_latlon_points", &s2point_index::get_latlon_points, py::call_guard<py::gil_scoped_release>());
+
+    py_s2pointindex.def(py::pickle(
+        [](s2point_index &idx) {
+            return idx.get_latlon_points();
+        },
+        [](xt::pytensor<double, 2> &points) {
+            //if (t.size() != 1)
+            //    throw std::runtime_error("Invalid state!");
+
+            s2point_index *idx = new s2point_index(points);
+
+            return idx;
+        }
+     ));
 
 }
