362f0277e239231103ef78c05410fb3449c9eaee
Benoit Bovy <benbovy@gmail.com>
2020-12-06 16:47:08 +0100

    try find s2 on windows

--- cmake/Finds2.cmake
+++ cmake/Finds2.cmake
@@ -49,7 +49,7 @@ find_path(s2_INCLUDE_DIR s2/s2cell.h
 find_library(s2_LIBRARY
   NAMES s2
   HINTS ${S2_ROOT}
-  PATH_SUFFIXES lib
+  PATH_SUFFIXES lib libs Library
   )
 
 find_package_handle_standard_args(s2
