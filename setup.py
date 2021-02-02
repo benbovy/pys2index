6e9b26f66a8fd3b311f03a5b85285913852c4bb1
Benoit Bovy <benbovy@gmail.com>
2021-02-02 13:35:48 +0100

    revert to dev version

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pys2index',
-    version='0.1.2',
+    version='0.2.0dev',
     author='Benoît Bovy',
     author_email='benbovy@gmail.com',
     url='https://github.com/benbovy/pys2index',
